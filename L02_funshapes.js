f0ef8ec448aec519f8990c8ab8fdb2c9082666af
Walden-pro <waldenchho@gmail.com>
2025-07-16 08:23:31 +0000

    Jul 16, 2025, 8:23 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -20,7 +20,7 @@ circle(300,200,200)
 fill(255,255,255)
 circle(210,100,65)
 fill(255,255,255)
-circle(210,100,65)
+circle(410,100,65)
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
