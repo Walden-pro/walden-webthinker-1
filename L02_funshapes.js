ec37a12d9de8390f59b752b1063d97da62735fa5
Walden-pro <waldenchho@gmail.com>
2025-07-16 08:10:51 +0000

    Jul 16, 2025, 8:10 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -9,9 +9,10 @@ function setup(){
 function draw() {
   // Challenge 1: An eye using ellipse() function
 fill(255,0,0)
-ellipse(300,200,100,100)
+ellipse(300,100,100,100)
   // Challenge 2: A face using circle() function
-
+fill(255,0,0)
+ellipse(300,100,100,100)
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
