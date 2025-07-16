eb018670f0eb587f4dc022b658f89e24d7092d3b
Walden-pro <waldenchho@gmail.com>
2025-07-16 08:49:44 +0000

    Jul 16, 2025, 8:49 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -39,17 +39,18 @@ function draw() {
 // fill(255,255,255)
 // ellipse(330,285,30,40)
   // Challenge 4: A rocketship using triangle() function
-fill(255,255,255)
-ellipse(300,215,225,300)
-fill(255,255,255)
-triangle(195,125,405,125,300,25)
-fill(255,255,255)
-circle(300,225,50)
-fill(255,255,255)
-rect(200,325,200,50)
+// fill(255,255,255)
+// ellipse(300,215,225,300)
+// fill(255,255,255)
+// triangle(195,125,405,125,300,25)
+// fill(255,255,255)
+// circle(300,225,50)
+// fill(255,255,255)
+// rect(200,325,200,50)
   // Challenge 5: A square face using quad() function
 
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
+  
 }
