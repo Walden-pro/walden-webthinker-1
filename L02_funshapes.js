7d7b1ec7dd12b568936950ec4808ff85ea8a207a
Walden-pro <waldenchho@gmail.com>
2025-07-16 08:44:27 +0000

    Jul 16, 2025, 8:44 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -44,7 +44,7 @@ ellipse(300,215,225,250)
 fill(255,255,255)
 triangle(195,150,405,150,300,25)
 fill(255,255,255)
-circle(195,150,405,150,300,25)
+circle(195,150,100)
   // Challenge 5: A square face using quad() function
 
   // Challenge 6: Add some colours to the drawings in the above challenges
