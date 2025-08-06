ff369c0a8ec12a5e39f68af59aa5eaeb0fc75637
Walden-pro <waldenchho@gmail.com>
2025-08-06 08:14:12 +0000

    Aug 6, 2025, 8:14 AM

--- note.js
+++ note.js
@@ -4,7 +4,7 @@
 //the 10 up there is the spacing between the shapes
 // to make the tab,press go live at the bottom.
 // to remove suggestions,click settings button and settings and click off suggestions
-
+rect(xpos,)
 
 
 
