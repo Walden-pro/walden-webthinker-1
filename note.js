9d549c5de96ad71827ed7be58fd4b86c390e4e84
Walden-pro <waldenchho@gmail.com>
2025-08-06 07:56:42 +0000

    Aug 6, 2025, 7:56 AM

--- note.js
+++ note.js
@@ -11,16 +11,16 @@
 
 
 
-
-// function setup() {
-//     createCanvas(1500, 700);
-//     background(220);
-//     fill(255,0,0)
-// }
-// function draw() {
-
-
+//SETUP
+function setup() {
+    createCanvas(1500, 700);
+    background(220);
+    fill(255,0,0)
+}
+function draw() {
 
 
 
-// }
+
+
+}
