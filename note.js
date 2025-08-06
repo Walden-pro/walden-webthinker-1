2a24389d249fa42200d0f9194473e3bea74a43d3
Walden-pro <waldenchho@gmail.com>
2025-08-06 07:56:32 +0000

    Aug 6, 2025, 7:56 AM

--- note.js
+++ note.js
@@ -4,3 +4,23 @@
 //the 10 up there is the spacing between the shapes
 // to make the tab,press go live at the bottom.
 // to remove suggestions,click settings button and settings and click off suggestions
+
+
+
+
+
+
+
+
+// function setup() {
+//     createCanvas(1500, 700);
+//     background(220);
+//     fill(255,0,0)
+// }
+// function draw() {
+
+
+
+
+
+// }
