dac9bfde94a9377f907eec2510ba0bdf80961fea
Walden-pro <waldenchho@gmail.com>
2025-08-06 08:26:12 +0000

    Aug 6, 2025, 8:26 AM

--- note.js
+++ note.js
@@ -5,7 +5,7 @@
 // to make the tab,press go live at the bottom.
 // to remove suggestions,click settings button and settings and click off suggestions
 // rect(xpos,ypos,rectsize,rectsize)
-
+// use === to compare a vs b
 
 
 
