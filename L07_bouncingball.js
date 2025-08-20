1c7f739d7f3d733a3219d8308356fe2753976fb7
Walden-pro <waldenchho@gmail.com>
2025-08-20 08:57:54 +0000

    Aug 20, 2025, 8:57 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -5,8 +5,16 @@ function setup() {
 }
 function draw() {
   triangle(0,200,200,200,100,0)
+}
 
 
+let xpos = 50;
+noStroke();
+for (let count = 0; count < 5; count++) {
+    fill(0);
+    if (count%2 === 0 ) fill(255);
+    rect(xpos,50,50,50);
+    xpos = xpos + 60;
 
 
 }
