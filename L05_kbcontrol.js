067be20ffe14f79c0ab3f32108aa16ced3fe7281
Walden-pro <waldenchho@gmail.com>
2025-08-20 08:00:40 +0000

    Aug 20, 2025, 8:00 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -31,22 +31,22 @@ rect(xp,yp ,80, 80);
 // textSize(48);
 // text(key, 50,40);
 // text(keyCode,50,80);
-if(keyIsDown(68)){
-xp = xp + 10;
-}
-if(keyIsDown(65)){
-xp = xp - 10;
-}
-xp = constrain(xp,40,width-40);
-if(keyIsDown(83)){
-yp = yp + 10;
-}
-if(keyIsDown(87)){
-yp = yp - 10;
-}
-yp = constrain(yp,40,height-40);
-fill(255,0,0);
-rect(xp,yp ,80,80);
+// if(keyIsDown(68)){
+// xp = xp + 10;
+// }
+// if(keyIsDown(65)){
+// xp = xp - 10;
+// }
+// xp = constrain(xp,40,width-40);
+// if(keyIsDown(83)){
+// yp = yp + 10;
+// }
+// if(keyIsDown(87)){
+// yp = yp - 10;
+// }
+// yp = constrain(yp,40,height-40);
+// fill(255,0,0);
+// rect(xp,yp ,80,80);
 noFill()
 rect(40,40,300,300)
 }
