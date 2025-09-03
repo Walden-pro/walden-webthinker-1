0dea4d139d823ba26030763a80e50aa22ce95a71
Walden-pro <waldenchho@gmail.com>
2025-09-03 07:29:51 +0000

    Sep 3, 2025, 7:29 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -2,30 +2,48 @@ let xp  = 0;;;
 let yp = 0;
 
 
-let si,se,bgm;
+let si,se,bgm,si2;
 
 function setup() {
     createCanvas(1535, 727);
     background(220);
+    bgm.loop();
 }
 function draw() {
 background("skyblue")
 image(si,xp, yp ,110,133)
-
-if(keyIsDown(RIGHT_ARROW)){
+image(si2,xp, yp ,110,133)
+if(keyIsDown(68)){
 xp = xp + 30;
 }
-if(keyIsDown(LEFT_ARROW)){
+if(keyIsDown(65)){
 xp = xp - 30;
 }
-if(keyIsDown(DOWN_ARROW)){
+if(keyIsDown(83)){
 yp = yp + 30;
 }
-if(keyIsDown(UP_ARROW)){
+if(keyIsDown(87)){
 yp = yp - 30;
 }
 if(keyIsDown(32)){
 
+}
+
+
+
+
+
+if(keyIsDown(39)){
+xp = xp + 30;
+}
+if(keyIsDown(37)){
+xp = xp - 30;
+}
+if(keyIsDown(40)){
+yp = yp + 30;
+}
+if(keyIsDown(38)){
+yp = yp - 30;
 }
 yp = constrain(yp,40,height-120);
 xp = constrain(xp,40,width-120);
@@ -33,6 +51,7 @@ xp = constrain(xp,40,width-120);
 function preload() {
 se = loadSound('assets/pop.mp3')
 si = loadImage('assets/pico-a.png')
+si2 = loadImage('assets/penguin.svg')
 bgm = loadSound('assets/brain.mp3')
 
 
