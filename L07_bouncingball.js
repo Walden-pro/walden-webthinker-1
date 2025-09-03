ffb0bac5f001a7e4c792726f31e7116bdd84e958
Walden-pro <waldenchho@gmail.com>
2025-09-03 08:09:42 +0000

    Sep 3, 2025, 8:09 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,36 +1,36 @@
-let xpos = 200;
-let ypos = 200;
+// let xpos = 200;
+// let ypos = 200;
 
 function setup() {
     createCanvas(600, 400);
     background("skyblue");
 }
 function draw() {
-background("skyblue");
-circle(xpos, ypos/2, 50);
-if(keyIsDown(LEFT_ARROW)){
-xpos = xpos - 5;
-}
-if ( xpos < 25){
-xpos = 25;
-}
-if(keyIsDown(RIGHT_ARROW)){
-    xpos = xpos + 5;
-}
+// background("skyblue");
+// circle(xpos, ypos/2, 50);
+// if(keyIsDown(LEFT_ARROW)){
+// xpos = xpos - 5;
+// }
+// if ( xpos < 25){
+// xpos = 25;
+// }
+// if(keyIsDown(RIGHT_ARROW)){
+//     xpos = xpos + 5;
+// }
 
-if ( xpos > width-25){
-xpos = width-25;
-}
-if(keyIsDown(UP_ARROW)){
-ypos = ypos - 5;
-}
+// if ( xpos > width-25){
+// xpos = width-25;
+// }
+// if(keyIsDown(UP_ARROW)){
+// ypos = ypos - 5;
+// }
 
-if(keyIsDown(DOWN_ARROW)){
-ypos = ypos + 5;
-}
-if ( ypos < 25){
-ypos = 25;
-}
+// if(keyIsDown(DOWN_ARROW)){
+// ypos = ypos + 5;
+// }
+// if ( ypos < 25){
+// ypos = 25;
+// }
 
 }
 
