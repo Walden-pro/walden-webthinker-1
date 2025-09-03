cb85d13359fec000bec3540059f41f1794f487ab
Walden-pro <waldenchho@gmail.com>
2025-09-03 07:55:35 +0000

    Sep 3, 2025, 7:55 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,14 +3,14 @@ let xpos = 200;
 
 function setup() {
     createCanvas(1535, 727);
-    background(220);
+    
 }
 function draw() {
 background("skyblue")
 circle(xpos,height/2,50);
 if(keyIsDown(LEFT_ARROW)){
 
-    
+
 }
 
 
