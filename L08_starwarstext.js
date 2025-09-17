9be3f7625f3c6df293b1dfa250b86bf793483c40
Walden-pro <waldenchho@gmail.com>
2025-09-17 08:10:34 +0000

    Sep 17, 2025, 8:10 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -15,7 +15,7 @@ function setup() {
 function draw() {
     let textY = 0;
 
-    translate(width/2, 0);
+    translate(width/2, -500);
     background("black");
     textAlign(CENTER, CENTER);
     scale(1,3);
