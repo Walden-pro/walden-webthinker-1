e2e0cb5b8981a98e36f8b77d8d0a5b06effff9d7
Walden-pro <waldenchho@gmail.com>
2025-09-17 08:05:42 +0000

    Sep 17, 2025, 8:05 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -24,7 +24,7 @@ function draw() {
     let story = [
         "Longggggggggggggggggggggggggggggggggggggggggggg time ago in a galaxy far...",
         "very very very very very very very very very very very very very very very very very very longgggggggggggggggggggggg ago", 
-        "",
+        "super extremely longgggggggggg ago not even close ",
         "",
         "",
         "There was a bunch of amonguses...",
