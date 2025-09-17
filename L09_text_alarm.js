ceeea4b172feda55315b0ab38c606f0b1d16e422
Walden-pro <waldenchho@gmail.com>
2025-09-17 08:51:02 +0000

    Sep 17, 2025, 8:51 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,19 +1,19 @@
-let xpos;
-let vx;
-let ypos;
-function setup() {
-    createCanvas(600, 400);
-    background(220);
-    fill("black")
-    textSize(48);
-    textAlign(CENTER, CENTER);
-    xpos = width/2;
-    vx = 50; }
-function draw() {
-    background(220);
-    translate(xpos, height/2);
-    text("Bounce!",0,0);
-    xpos = xpos + vx
-    if(xpos > width||xpos < 0){
-        vx = vx * -1
-    } }
+// let xpos;
+// let vx;
+// let ypos;
+// function setup() {
+//     createCanvas(600, 400);
+//     background(220);
+//     fill("black")
+//     textSize(48);
+//     textAlign(CENTER, CENTER);
+//     xpos = width/2;
+//     vx = 50; }
+// function draw() {
+//     background(220);
+//     translate(xpos, height/2);
+//     text("Bounce!",0,0);
+//     xpos = xpos + vx
+//     if(xpos > width||xpos < 0){
+//         vx = vx * -1
+//     } }
