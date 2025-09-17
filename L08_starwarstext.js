36d2e3899cdfeef6d6a846b2bfc2d1562b7592ab
Walden-pro <waldenchho@gmail.com>
2025-09-17 07:33:50 +0000

    Sep 17, 2025, 7:33 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -14,7 +14,7 @@ function setup() {
 }
 function draw() {
     background("black");
-    text("Longgggg time ago in a galaxy far...",0,50)
+    text("Longgggg time ago in a galaxy far",0,50)
 if (keyIsDown(32)){
 sfx.play();
 }
