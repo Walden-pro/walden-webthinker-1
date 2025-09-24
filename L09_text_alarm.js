c39490197db1795e533a7fd8f986be4e7550bb8f
Walden-pro <waldenchho@gmail.com>
2025-09-24 09:00:58 +0000

    Sep 24, 2025, 9:00 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -82,6 +82,7 @@ function dosomething(){
     if(counter === 0){
         clearInterval(objectid);
         sfx.play();
+        
             // for(let index = 0;index < 10;index++){
 
             //     bgcolor = color(random(255), random(255),random(255));
