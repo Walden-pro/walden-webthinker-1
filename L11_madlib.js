d92f62474b938d7b1ca75911b1319ffa11e7a92b
Walden-pro <waldenchho@gmail.com>
2025-10-08 08:57:16 +0000

    Oct 8, 2025, 8:57 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -44,8 +44,8 @@ function updatestory() {
 // console.log(inputadverb.value())
 // console.log(inputplace.value())
 
-text(inputnoun.value(),inputverb.value(),inputadjective.value(),inputadverb.value(),inputplace.value()
-)
+text(inputnoun.value(),inputverb.value(),inputadjective.value(),inputadverb.value(),inputplace.value())
+
 
 
 //      background(220);
