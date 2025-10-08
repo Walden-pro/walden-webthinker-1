f3010fd4872b96733acfb07da58f14694742ed84
Walden-pro <waldenchho@gmail.com>
2025-10-08 08:40:19 +0000

    Oct 8, 2025, 8:40 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -16,7 +16,7 @@ button1.mousePressed(updatestory);
 
 }
 function updatestory() {
-console.log(inputNoun.value())
+console.log(inputnoun.value())
 //      background(220);
 // story = ist.value();
 // textAlign(CENTER,CENTER);
