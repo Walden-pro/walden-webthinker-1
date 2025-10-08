3d02e83f0de0e3af2a595ba0f03a8c0c9b11fa55
Walden-pro <waldenchho@gmail.com>
2025-10-08 08:45:50 +0000

    Oct 8, 2025, 8:45 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -23,13 +23,13 @@ inputadjective = createInput(story,"text");
 inputadjective.position(350,150);
 inputadjective.size(350);
 
-inputverb = createInput(story,"text");
-inputverb.position(350,100);
-inputverb.size(350);
+inputadverb = createInput(story,"text");
+inputadverb.position(350,100);
+inputadverb.size(350);
 
 inputadjective = createInput(story,"text");
 inputadjective.position(350,150);
-inputadjective.size(350);
+inputplace.size(350);
 
 button1 = createButton("Generate story");
 button1.position(350,500);
