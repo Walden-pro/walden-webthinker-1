3a2f86c1c996c077c3797bcc170a654589cec2c2
Walden-pro <waldenchho@gmail.com>
2025-10-08 08:14:35 +0000

    Oct 8, 2025, 8:14 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -3,7 +3,7 @@ let button1;
 let story;
 
 function setup() {
-    createCanvas(1535, 727);
+    createCanvas(600, 600);
     background(220);
 ist = createInput(story,"text");
 ist.position(50,50);
