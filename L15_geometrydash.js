730a4ab2e070c69073c05630bdc3b4a1fae97899
Walden-pro <waldenchho@gmail.com>
2025-10-29 08:53:11 +0000

    Oct 29, 2025, 8:53 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -17,7 +17,7 @@ function draw() {
     drawSpike()
 }
 function drawSpike(x1,x2,x3,y1,y2,y3) {
-    x1=
+    x1=805,x2=610
 // fill("red")
 // triangle(100,100,100,100,100,100)
 triangle(805,610,780,675,830,675)
