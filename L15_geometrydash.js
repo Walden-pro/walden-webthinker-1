3d01ec5edd8f4d57dec1ada3b71f56b344eae079
Walden-pro <waldenchho@gmail.com>
2025-10-29 08:30:45 +0000

    Oct 29, 2025, 8:30 AM

--- L15_geometrydash.js
+++ L15_geometrydash.js
@@ -1,10 +1,11 @@
 let ypos = height/2;
 function setup() {
     createCanvas(1535, 727);
-    background("black");
+    
     
 }
 function draw() {
+    background("black");
     fill("darkblue");
     rect(0,675,1540,56);
     fill("limegreen")
